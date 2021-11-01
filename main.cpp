06e731effda22f849b6b9c7854d5c56bb610d6d8
OldScripter <Oldscripter@gmail.com>
2021-11-02 01:44:06 +0300

    Intermediate commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <map>
+
+std::string getArticleFromCin()
+{
+    std::cout << "Please enter the article.\n";
+    std::string value;
+    std::getline(std::cin, value);
+    return value;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::map<std::string, int> shop;
+    std::map<std::string, int> cart;
+
+    //Add good to shop:
+    std::string command;
+    std::cout << "Enter the goods for shop:\n";
+    while (command != "next")
+    {
+
+    }
+
     return 0;
 }
